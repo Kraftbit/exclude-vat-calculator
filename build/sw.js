137390f3dbd46f8759655972450b3f9e4789b857
Apatrid <mijodz@gmail.com>
2021-08-04 15:07:40 +0200

    added README

--- build/sw.js
+++ build/sw.js
@@ -22,7 +22,7 @@ if (workbox) {
     workbox.precaching.precacheAndRoute([
   {
     "url": "index.html",
-    "revision": "59e7b89db43d9ffbebbd931dde79809a"
+    "revision": "af428555256d73667fa543c7a781d773"
   },
   {
     "url": "css/style.css",
@@ -152,7 +152,7 @@ if (workbox) {
     );
 
     const articleHandler = workbox.strategies.networkFirst({
-        cacheName: 'articles-cache',
+        cacheName: 'index-cache',
         plugins: [
             new workbox.expiration.Plugin({
                 maxEntries: 50,
@@ -160,7 +160,7 @@ if (workbox) {
         ]
     });
 
-    workbox.routing.registerRoute(/(.*)article(.*)\.html/, args => {
+    workbox.routing.registerRoute(/(.*)index(.*)\.html/, args => {
         return articleHandler.handle(args).then(response => {
             if (!response) {
                 return caches.match('pages/offline.html');
@@ -171,26 +171,6 @@ if (workbox) {
         });
     });
 
-    const postHandler = workbox.strategies.cacheFirst({
-        cacheName: 'posts-cache',
-        plugins: [
-            new workbox.expiration.Plugin({
-                maxEntries: 50,
-            })
-        ]
-    });
-
-    workbox.routing.registerRoute(/(.*)pages\/post(.*)\.html/, args => {
-        return postHandler.handle(args).then(response => {
-            if (response.status === 404) {
-                return caches.match('pages/404.html');
-            }
-            return response;
-        })
-            .catch(function () {
-                return caches.match('pages/offline.html');
-            });
-    });
 
 } else {
     console.log(`Boo! Workbox didn't load 😬`);
--- src/sw.js
+++ src/sw.js
@@ -35,7 +35,7 @@ if (workbox) {
     );
 
     const articleHandler = workbox.strategies.networkFirst({
-        cacheName: 'articles-cache',
+        cacheName: 'index-cache',
         plugins: [
             new workbox.expiration.Plugin({
                 maxEntries: 50,
@@ -43,7 +43,7 @@ if (workbox) {
         ]
     });
 
-    workbox.routing.registerRoute(/(.*)article(.*)\.html/, args => {
+    workbox.routing.registerRoute(/(.*)index(.*)\.html/, args => {
         return articleHandler.handle(args).then(response => {
             if (!response) {
                 return caches.match('pages/offline.html');
@@ -54,26 +54,6 @@ if (workbox) {
         });
     });
 
-    const postHandler = workbox.strategies.cacheFirst({
-        cacheName: 'posts-cache',
-        plugins: [
-            new workbox.expiration.Plugin({
-                maxEntries: 50,
-            })
-        ]
-    });
-
-    workbox.routing.registerRoute(/(.*)pages\/post(.*)\.html/, args => {
-        return postHandler.handle(args).then(response => {
-            if (response.status === 404) {
-                return caches.match('pages/404.html');
-            }
-            return response;
-        })
-            .catch(function () {
-                return caches.match('pages/offline.html');
-            });
-    });
 
 } else {
     console.log(`Boo! Workbox didn't load 😬`);
